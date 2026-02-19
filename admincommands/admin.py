2d0c93c76cad66a7c54a28c3038c8e150af592d1
Manon Hardy <hardy.m4n0n@gmail.com>
2026-02-19 13:17:03 +0100

    NIQUE

--- admincommands/admin.py
+++ admincommands/admin.py
@@ -3,8 +3,3 @@ from discord.ext import commands
 @commands.has_permissions(administrator=True)
 async def run(bot, message, args):
     await message.channel.send("c bon t admin")
-
-@run.error
-async def run_error(bot, message, error):
-    if isinstance(error, commands.MissingPermissions):
-        await message.channel.send("t pa admin loser")
