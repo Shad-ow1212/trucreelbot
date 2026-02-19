2f932c8f1e43bb39f42e666696d423356cec707d
Manon Hardy <hardy.m4n0n@gmail.com>
2026-02-19 12:54:20 +0100

    fix

--- admincommands/admin.py
+++ admincommands/admin.py
@@ -1,9 +1,10 @@
-import discord
-
+from discord.ext import commands
+#fonction de test pour vérifier la perm admin en vue d'ajouter des commandes admin
 @commands.has_permissions(administrator=True)
 async def run(bot, message, args):
     await message.channel.send("c bon t admin")
 
-@commands.has_permissions(administrator=False)
-async def run(bot, message, args):
-    await message.channel.send("t pa admin loser")
+@run.error
+async def run_error(bot, message, error):
+    if isinstance(error, commands.MissingPermissions):
+        await message.channel.send("t pa admin loser")
